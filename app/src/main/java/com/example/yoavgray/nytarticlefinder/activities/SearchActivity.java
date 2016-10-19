e42f3575484d6c07f2074a25f8bf9e7ac6bee692
Yoav Gray <yoav@autolist.com>
2016-10-18 23:29:37 -0700

    Cleaned methods and added Calligraphy

--- app/src/main/java/com/example/yoavgray/nytarticlefinder/activities/SearchActivity.java
+++ app/src/main/java/com/example/yoavgray/nytarticlefinder/activities/SearchActivity.java
@@ -1,27 +1,38 @@
 package com.example.yoavgray.nytarticlefinder.activities;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.yoavgray.nytarticlefinder.R;
 import com.example.yoavgray.nytarticlefinder.adapters.ArticleAdapter;
 import com.example.yoavgray.nytarticlefinder.models.Article;
+import com.example.yoavgray.nytarticlefinder.utils.DividerItemDecoration;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import okhttp3.Call;
@@ -30,30 +41,78 @@ import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class SearchActivity extends AppCompatActivity {
     public static final String ARTICLE_API_KEY = "58b8ef6b492349d4b3a3c2968d411aa6";
     public final static String NYTIMES_URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-    Article[] articles;
+    List<Article> articles;
     ArticleAdapter adapter;
-    OkHttpClient client = new OkHttpClient();
+    OkHttpClient client;
 
     @BindView(R.id.app_search_bar) Toolbar appSearchBar;
-    @BindView(R.id.movies_recycler_view) RecyclerView articlesRecyclerView;
+    @BindView(R.id.articles_recycler_view) RecyclerView articlesRecyclerView;
     @BindView(R.id.swipe_refresh_container) SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.layout_activity_search) RelativeLayout searchActivityLayout;
+    @BindString(R.string.retry_caps) String retryString;
+    @BindString(R.string.load_articles_error) String loadArticlesErrorString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
-        setupToolBar();
-        client = new OkHttpClient();
+        setCalligraphy();
 
+        client = new OkHttpClient();
+        articles = new ArrayList<>();
+        // Setup Layouts
+        setupToolBar();
+        setRecyclerView();
+        setupSwipeRefreshLayout();
+
+        checkConnectivity();
         //TODO: remember last search
         loadArticles();
+    }
 
+    /*
+        Set Calligraphy fonts for the activity!
+     */
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    private void setCalligraphy() {
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Calligraffiti.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+
+    /**
+     * This method sets the Toolbar on start
+     */
+    private void setupToolBar() {
+        setSupportActionBar(appSearchBar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Enable the Up button
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("");
+        }
+    }
+
+    /**
+     * This method sets the SwipeRefreshLayout on start
+     */
+    private void setupSwipeRefreshLayout() {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -67,6 +126,48 @@ public class SearchActivity extends AppCompatActivity {
                 android.R.color.holo_red_light);
     }
 
+    /**
+     * This method sets the RecyclerView on start
+     */
+    private void setRecyclerView() {
+        adapter = new ArticleAdapter(this, articles);
+        articlesRecyclerView.setAdapter(adapter);
+        StaggeredGridLayoutManager gridLayoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        // Attach the layout manager to the recycler view
+        articlesRecyclerView.setLayoutManager(gridLayoutManager);
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(getBaseContext(), DividerItemDecoration.VERTICAL_LIST);
+        articlesRecyclerView.addItemDecoration(itemDecoration);
+    }
+
+    /**
+     * This method checks connectivity and renders a Snackbar if there's a problem
+     */
+    private void checkConnectivity() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = (activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting());
+        if (!isConnected) {
+            Snackbar
+                .make(searchActivityLayout,
+                        loadArticlesErrorString,
+                        Snackbar.LENGTH_INDEFINITE)
+                .setAction(retryString, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loadArticles();
+                    }
+                })
+            .setActionTextColor(Color.RED).show();
+        }
+    }
+
+    /**
+     * This method is used to load articles on start, refresh and retries if internet connection
+     * was not available
+     */
     private void loadArticles() {
         HttpUrl.Builder urlBuilder = HttpUrl.parse(NYTIMES_URL).newBuilder();
 //        urlBuilder.addQueryParameter("rsz", "8");
@@ -81,6 +182,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
+                checkConnectivity();
             }
 
             @Override
@@ -91,30 +193,20 @@ public class SearchActivity extends AppCompatActivity {
                     Gson gson = new GsonBuilder().create();
                     JsonArray jsonDocs = gson.fromJson(response.body().string(), JsonObject.class)
                             .getAsJsonObject("response").getAsJsonArray("docs");
-                    articles = gson.fromJson(jsonDocs, Article[].class);
+                    Article[] articlesArray = gson.fromJson(jsonDocs, Article[].class);
 
+                    // Should not assign a new reference, but act on this local reference of the data
+                    articles.addAll(new ArrayList<>(Arrays.asList(articlesArray)));
+                    // Cannot change Views state outside of the UI thread
                     SearchActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            adapter = new ArticleAdapter(getBaseContext(), Arrays.asList(articles));
-                            articlesRecyclerView.setAdapter(adapter);
-                            articlesRecyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+                            adapter.notifyDataSetChanged();
                         }
                     });
                 }
             }
         });
-    }
-
-    private void setupToolBar() {
-        setSupportActionBar(appSearchBar);
-        // Get a support ActionBar corresponding to this toolbar
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            // Enable the Up button
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle("");
-        }
     }
 
     @Override
